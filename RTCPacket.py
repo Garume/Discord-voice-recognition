5c0430e152346225f920bd67aceaf36e378957b8
desuboruto <desuboruto@gmail.com>
2021-12-07 02:27:46 +0900

    Packet保管

--- RTCPacket.py
+++ RTCPacket.py
@@ -1,3 +1,5 @@
+import struct,time
+
 class RTCPacket:
     def __init__(self, header, decrypted):
         self.version = (header[0] & 0b11000000) >> 6
@@ -15,4 +17,23 @@ class RTCPacket:
 
         self.header = header
         self.decrypted = decrypted
-        self.seq, self.timestamp, self.ssrc = struct.unpack_from('>HII', header, 2)    
+        self.seq, self.timestamp, self.ssrc = struct.unpack_from('>HII', header, 2)  
+
+    def set_real_time(self):
+        self.real_time = time.time()
+
+    def  calc_extension_header_length(self) -> None:
+        if not (self.decrypted[0] == 0xbe and self.decrypted[1] == 0xde and len(self.decrypted) > 4):
+            return
+        self.ext_length = int.from_bytes(self.decrypted[2:4],"big")
+        offset=4
+        for i in range(self.ext_length):
+            byte_ = self.decrypted[offset]
+            offset += 1
+            if byte_ == 0:
+                continue
+            offset += 1 + (0b1111&(byte_ >> 4))
+        
+        if self.decrypted[offset + 1] in [0, 2]:
+            offset += 1
+        self.decrypted = self.decrypted[offset + 1:]
--- VoiceSocket.py
+++ VoiceSocket.py
@@ -2,6 +2,8 @@ from discord import VoiceClient
 from discord.gateway import DiscordVoiceWebSocket
 import nacl.secret
 
+from RTCPacket import RTCPacket
+
 class MyVoiceWebSocket(DiscordVoiceWebSocket):
     def __init__(self, socket, loop):
         super().__init__(socket, loop)
@@ -30,6 +32,13 @@ class MyVoiceClient(VoiceClient):
             recv = await self.loop.sock_recv(self.socket,2**16)
             print(recv)
 
+            if 200 <= recv[1] < 205:
+                continue
+            decrypt_func = getattr(self,f'decrypt_{self.mode}')
+            header,data = decrypt_func(recv)
+            packet = RTCPacket(header,data)
+
+
     async def connect_websocket(self) -> MyVoiceWebSocket:
         ws = await MyVoiceWebSocket.from_client(self)
         self._connected.clear()
@@ -50,4 +59,24 @@ class MyVoiceClient(VoiceClient):
             header, encrypted = data[:12], data[12:]
             nonce = bytearray(24)
             nonce[:12] = header
+        return header, box.decrypt(bytes(encrypted), bytes(nonce))
+
+    def decrypt_xsalsa20_poly1305_suffix(self, data: bytes) -> tuple:
+        box = nacl.secret.SecretBox(bytes(self.secret_key))
+        is_rtcp = 200 <= data[1] < 205
+        if is_rtcp:
+            header, encrypted, nonce = data[:8], data[8:-24], data[-24:]
+        else:
+            header, encrypted, nonce = data[:12], data[12:-24], data[-24:]
+        return header, box.decrypt(bytes(encrypted), bytes(nonce))
+
+    def decrypt_xsalsa20_poly1305_lite(self, data: bytes) -> tuple:
+        box = nacl.secret.SecretBox(bytes(self.secret_key))
+        is_rtcp = 200 <= data[1] < 205
+        if is_rtcp:
+            header, encrypted, _nonce = data[:8], data[8:-4], data[-4:]
+        else:
+            header, encrypted, _nonce = data[:12], data[12:-4], data[-4:]
+        nonce = bytearray(24)
+        nonce[:4] = _nonce
         return header, box.decrypt(bytes(encrypted), bytes(nonce))
